cd17c9c69eb69c86e935cd9c86168a4dccf54b8a
Safin Bhuiyan <112971550+SafinBhuiyan@users.noreply.github.com>
2022-11-01 23:39:12 +0600

    Add files via upload

--- hello.c
+++ hello.c
@@ -1,6 +1,21 @@
-#include<stdio.h>
-int main(){
-  printf("Hello World!");
-  
-  return 0;
+#include <stdio.h>
+
+int main() {
+    int num1,num2;
+
+    printf("Please enter a number:-");
+    scanf("%d",&num1);
+    printf("Please enter another number:-");
+    scanf("%d",&num2);
+    
+    int sum= num1+num2;
+    printf("Sum= %d \n",sum);
+    int abs= num1-num2;
+    printf("Abs= %d \n",abs);
+    int mul= num1*num2;
+    printf("Mul= %d \n",mul);
+    float div= num1/num2;
+    printf("Div= %f \n",div);
+
+    return 0;
 }
